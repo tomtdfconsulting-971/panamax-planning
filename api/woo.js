7e154e9e6a15a0658d16adfa65252906637231c5
tomtdfconsulting-971 <tom.tdfconsulting@gmail.com>
2026-09-09 18:48:30 -0400

    Update woo.js

--- api/woo.js
+++ api/woo.js
@@ -4,7 +4,7 @@ import { verifyIdToken, loadUsers } from './_firebase.js';
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
